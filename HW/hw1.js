74d6dd36c223504913e8ca49273a06e79a46f516
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-02-04 13:13:36 -0600

    3 HW

--- HW/hw1.js
+++ HW/hw1.js
@@ -6,23 +6,30 @@
 // console.log(madlib("Daniel", "philosophy"));
 
 //2. Tip Calculator
-function tipAmount(bill, service) {
-  switch (service) {
-    case "good":
-      tip = bill * 0.2;
-      return tip;
-      break;
-    case "fair":
-      tip = bill * 0.15;
-      return tip;
-      break;
-    case "bad":
-      tip = bill * 0.1;
-      return tip;
-      break;
-    default:
-      return "Enter good, fair, or bad for service.";
-      break;
-  }
-}
-console.log(tipAmount(20, "good"));
+// function tipAmount(bill, service) {
+//   switch (service) {
+//     case "good":
+//       tip = bill * 0.2;
+//       return tip;
+//       break;
+//     case "fair":
+//       tip = bill * 0.15;
+//       return tip;
+//       break;
+//     case "bad":
+//       tip = bill * 0.1;
+//       return tip;
+//       break;
+//     default:
+//       return "Enter good, fair, or bad for service.";
+//       break;
+//   }
+// }
+// console.log(tipAmount(20, "good"));
+
+//3. Tip Calculator 2
+// function totalAmount(bill, service) {
+//   total = tipAmount(bill, service) + bill;
+//   return total;
+// }
+// console.log(totalAmount(100, "good"));
