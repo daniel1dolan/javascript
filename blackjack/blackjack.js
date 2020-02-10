1f86b0124d9c28009351cecc1e254a27a93bd105
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-02-10 13:58:49 -0600

    finished blackjack

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,6 +1,9 @@
 let playerHandD = document.querySelector("#player-hand");
 let dealerHandD = document.querySelector("#dealer-hand");
 let dealButton = document.querySelector("#deal-button");
+let hitButton = document.querySelector("#hit-button");
+let messagesD = document.querySelector("#messages");
+let standButton = document.querySelector("#stand-button");
 console.log(dealButton);
 console.log(dealerHandD);
 console.log(playerHandD);
@@ -8,7 +11,11 @@ console.log(playerHandD);
 class Deck {
   constructor() {
     this.deck = [];
-
+    this.reset();
+    this.shuffle();
+  }
+  reset() {
+    this.deck = [];
     const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
     const values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "King", "Queen"];
 
@@ -22,7 +29,6 @@ class Deck {
       }
     }
   }
-
   shuffle() {
     const { deck } = this;
     for (var i = deck.length - 1; i > 0; i--) {
@@ -38,36 +44,182 @@ class Deck {
     return this.deck.pop();
   }
 }
-let dealerHand = [];
-let playerHand = [];
 
+class Player {
+  constructor() {
+    this.hand = [];
+    this.points = 0;
+  }
+  checkBust() {
+    if (this.points > 21) {
+      let bustMessage = document.createElement("div");
+      bustMessage.className = "alert";
+      bustMessage.innerHTML =
+        '<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> <strong>You Busted!</strong>';
+      messagesD.appendChild(bustMessage);
+      reseto();
+    }
+  }
+}
+
+class Dealer {
+  constructor() {
+    this.hand = [];
+    this.points = 0;
+  }
+  checkBust() {
+    if (this.points > 21) {
+      let bustMessage1 = document.createElement("div");
+      bustMessage1.className = "alert success";
+      bustMessage1.innerHTML =
+        '<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> <strong>Dealer Busted! You win!</strong>';
+      messagesD.appendChild(bustMessage1);
+      reseto();
+    } else if (this.points == player.points) {
+      let bustMessage2 = document.createElement("div");
+      bustMessage2.className = "alert info";
+      bustMessage2.innerHTML =
+        '<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> <strong>Draw!</strong>';
+      messagesD.appendChild(bustMessage2);
+      reseto();
+    } else if (this.points > player.points) {
+      let bustMessage3 = document.createElement("div");
+      bustMessage3.className = "alert";
+      bustMessage3.innerHTML =
+        '<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> <strong>Dealer Wins!</strong>';
+      messagesD.appendChild(bustMessage3);
+      reseto();
+    } else if (this.points < player.points) {
+      let bustMessage4 = document.createElement("div");
+      bustMessage4.className = "alert success";
+      bustMessage4.innerHTML =
+        '<span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span> <strong>You Win!</strong>';
+      messagesD.appendChild(bustMessage4);
+      reseto();
+    }
+  }
+  hitLogic() {
+    while (this.points < 17) {
+      this.hand.push(deck1.deal());
+      let img = document.createElement("img");
+      img.setAttribute("src", dealer.hand[dealer.hand.length - 1]["imgFile"]);
+      img.setAttribute("width", "100px");
+      dealerHandD.appendChild(img);
+      calculatePoints(this);
+      calculatePoints(this);
+    }
+    calculatePoints(this);
+    calculatePoints(this);
+    calculatePoints(player);
+    this.checkBust();
+  }
+}
+
+const player = new Player();
+const dealer = new Dealer();
 const deck1 = new Deck();
+deck1.shuffle();
 
 dealButton.addEventListener("click", function(e) {
   console.log(e);
-  playerCards = [];
-  dealerCards = [];
-  playerCards.push(deck1.deal());
-  playerCards.push(deck1.deal());
-  dealerCards.push(deck1.deal());
-  dealerCards.push(deck1.deal());
-  playerHand.push(playerCards);
-  dealerHand.push(dealerCards);
-  console.log(playerCards);
-  console.log(dealerCards);
-  for (let i = 0; i < playerCards.length; i++) {
+  player.hand.push(deck1.deal());
+  player.hand.push(deck1.deal());
+  dealer.hand.push(deck1.deal());
+  dealer.hand.push(deck1.deal());
+  for (let i = 0; i < player.hand.length; i++) {
     let img = document.createElement("img");
-    img.setAttribute("src", playerCards[i].imgFile);
+    img.setAttribute("src", player.hand[i].imgFile);
     img.setAttribute("width", "100px");
     playerHandD.appendChild(img);
   }
-  for (let i = 0; i < dealerCards.length; i++) {
+  for (let i = 0; i < dealer.hand.length; i++) {
     let img = document.createElement("img");
-    img.setAttribute("src", dealerCards[i]["imgFile"]);
+    img.setAttribute("src", dealer.hand[i]["imgFile"]);
     img.setAttribute("width", "100px");
     dealerHandD.appendChild(img);
   }
+  calculatePoints(player);
+  calculatePoints(dealer);
+  console.log(player);
+  console.log(dealer);
 });
+
+hitButton.addEventListener("click", function(e) {
+  console.log(e);
+  player.hand.push(deck1.deal());
+  let img = document.createElement("img");
+  img.setAttribute("src", player.hand[player.hand.length - 1].imgFile);
+  img.setAttribute("width", "100px");
+  playerHandD.appendChild(img);
+  calculatePoints(player);
+  calculatePoints(dealer);
+  calculatePoints(player);
+  calculatePoints(dealer);
+  player.checkBust();
+});
+
+standButton.addEventListener("click", function(e) {
+  dealer.hitLogic();
+});
+
+function calculatePoints(person) {
+  let playerPointsD = document.querySelector("#player-points");
+  let dealerPointsD = document.querySelector("#dealer-points");
+  let pointsC = 0;
+  for (let i = 0; i < person.hand.length; i++) {
+    switch (person.hand[i]["points"]) {
+      case "Ace":
+        if (pointsC <= 10) {
+          pointsC += 11;
+        } else if (person.points == 21) {
+          pointsC += 11;
+        } else if (person.points > 21) {
+          pointsC += 1;
+        } else {
+          pointsC += 1;
+        }
+        break;
+      case "Jack":
+        pointsC += 10;
+        break;
+      case "Queen":
+        pointsC += 10;
+        break;
+      case "King":
+        pointsC += 10;
+        break;
+      default:
+        pointsC += person.hand[i]["points"];
+        break;
+    }
+  }
+  person.points = pointsC;
+  if (person == player) {
+    playerPointsD.textContent = person.points;
+  } else {
+    dealerPointsD.textContent = person.points;
+  }
+}
+
+function reseto() {
+  let x = document.querySelector(".closebtn");
+  let playerPointsD = document.querySelector("#player-points");
+  let dealerPointsD = document.querySelector("#dealer-points");
+  x.addEventListener("click", function(e) {
+    player.points = 0;
+    dealer.points = 0;
+    player.hand = [];
+    dealer.hand = [];
+    playerHandD.innerHTML = "";
+    dealerHandD.innerHTML = "";
+    playerPointsD.innerHTML = "";
+    dealerPointsD.innerHTML = "";
+    messagesD.innerHTML = "";
+    deck1.reset();
+    deck1.shuffle();
+  });
+}
+
 // deck1.shuffle();
 // console.log(deck1.deck);
 // deck1.shuffle();
