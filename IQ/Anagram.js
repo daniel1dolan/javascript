eef06b8e1db6b58528045acc9beac05e38df0f7b
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-06-18 15:23:00 -0500

    anagram checker algorithm

---
+++ IQ/Anagram.js
@@ -0,0 +1,29 @@
+function anagramChecker(fw, sw) {
+  let fwLetters = {};
+  let swLetters = {};
+  if (fw.length !== sw.length) {
+    return false;
+  }
+  fw.split("").forEach((letter) => {
+    if (fwLetters.hasOwnProperty(letter)) {
+      fwLetters[letter] += 1;
+    } else {
+      fwLetters[letter] = 1;
+    }
+  });
+  sw.split("").forEach((letter) => {
+    if (swLetters.hasOwnProperty(letter)) {
+      swLetters[letter] += 1;
+    } else {
+      swLetters[letter] = 1;
+    }
+  });
+  Object.keys(fwLetters).forEach((letter) => {
+    if (swLetters[letter] !== letter) {
+      return false;
+    }
+  });
+  return true;
+}
+
+console.log(anagramChecker("angel", "anglel"));
