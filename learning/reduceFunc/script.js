57eb86f21f9c9c1c4f02172cff369aba09c7b861
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-06-23 13:53:41 -0500

    fizzbuzz most concise and in browser

--- learning/reduceFunc/script.js
+++ learning/reduceFunc/script.js
@@ -5,3 +5,15 @@ document.getElementById("demo").innerHTML = numbers.reduce(myFunc);
 function myFunc(accumulator, num) {
   return accumulator - num;
 }
+
+let fizzBuzzList = [];
+
+function fizzBuzz() {
+  for (let i = 0; i < 100; ) {
+    fizzBuzzList.push((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
+  }
+}
+
+fizzBuzz();
+
+document.getElementById("fizzbuzz").innerHTML = fizzBuzzList;
