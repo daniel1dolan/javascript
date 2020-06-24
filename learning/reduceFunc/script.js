56554e2be46bba88fdd036efae2de3a87e866ae2
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-06-24 14:18:41 -0500

    dom derusting

--- learning/reduceFunc/script.js
+++ learning/reduceFunc/script.js
@@ -1,19 +1,41 @@
-let numbers = [150, 50, 10];
+// let numbers = [150, 50, 10];
 
-document.getElementById("demo").innerHTML = numbers.reduce(myFunc);
+// document.getElementById("demo").innerHTML = numbers.reduce(myFunc);
 
-function myFunc(accumulator, num) {
-  return accumulator - num;
+// function myFunc(accumulator, num) {
+//   return accumulator - num;
+// }
+
+// let fizzBuzzList = [];
+
+// function fizzBuzz() {
+//   for (let i = 0; i < 100; ) {
+//     fizzBuzzList.push((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
+//   }
+// }
+
+// fizzBuzz();
+
+// document.getElementById("fizzbuzz").innerHTML = fizzBuzzList;
+
+// let content = document.querySelector(".mainContent");
+// content.textContent = "Hello World";
+// content.setAttribute(
+//   "style",
+//   "color: blue; font-size: 100px; font-family: Helvetica"
+// );
+
+// let content = document.querySelector(".mainContent");
+// let newElement = document.createElement("span");
+// newElement.textContent = "My name is Daniel";
+// content.appendChild(newElement);
+
+function createElement(classOfParent, tagType, tagTextString) {
+  let parent = document.querySelector("." + classOfParent);
+  let newTag = document.createElement(`${tagType}`);
+  newTag.textContent = tagTextString;
+  parent.appendChild(newTag);
 }
 
-let fizzBuzzList = [];
-
-function fizzBuzz() {
-  for (let i = 0; i < 100; ) {
-    fizzBuzzList.push((++i % 3 ? "" : "fizz") + (i % 5 ? "" : "buzz") || i);
-  }
-}
-
-fizzBuzz();
-
-document.getElementById("fizzbuzz").innerHTML = fizzBuzzList;
+createElement("mainContent", "h1", "Hello World");
+createElement("mainContent", "span", "This is a span tag");
