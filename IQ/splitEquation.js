b3ecc9b344cfc72969675c99ad5a2356473c6996
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-07-06 13:25:57 -0500

    added split function for brackets

--- IQ/splitEquation.js
+++ IQ/splitEquation.js
@@ -1,3 +1,4 @@
+// 1. FIRST ATTEMPT
 let equation = "4+4-5*6/5";
 
 // let splitter = (equation) => {
@@ -7,23 +8,46 @@ let equation = "4+4-5*6/5";
 
 // console.log(splitter(equation));
 
+// 2. SECOND ATTEMPT
+
 let additionEquation = "1+2+3+56";
+let multiplicationEquation = "12*5+5*32+3";
+// ["12*5+5*32+3"] => ["12*5", "5*32", "3"] => .mapFunction (eachindex)
+// [60, 160, 3] => 223
+let minusEquation = "12*5-5*32+3";
+// String split by plus, call minus separator on each index,
+// minus operator splits on - sign and calls multiplication to
+// split on multiplaction sign and run multiplaction
+
+let parseMultiplicationSeparatedExpression = (expression) => {
+  const numberString = expression.split("*");
+  const numbers = numberString.map((noStr) => parseInt(noStr));
+  const initVal = 1.0;
+  const result = numbers.reduce((acc, num) => acc * num, initVal);
+  return result;
+};
+
+let parseMinusSeparatedExpression = (expression) => {
+  const numbersString = expression.split("-");
+  const numbers = numbersString.map((noStr) =>
+    parseMultiplicationSeparatedExpression(noStr)
+  );
+  const initVal = numbers[0];
+  const result = numbers.splice(1).reduce((acc, num) => acc - num, initVal);
+  return result;
+};
 
 let parsePlusSeparatedExpression = (expression) => {
-  // Convert a string to numbers after splitting them on operator.
   const numbersString = expression.split("+"); // ["123", "58", "49"]
   console.log(numbersString);
-
-  // Converts string numbers to individual numbers
-  const numbers = numbersString.map((noStr) => parseInt(noStr));
+  const numbers = numbersString.map((noStr) =>
+    parseMinusSeparatedExpression(noStr)
+  );
   console.log(numbers);
-
-  // Accumulates the addition of all the numbers which were
-  // split on the operator.
   const initVal = 0.0;
   const result = numbers.reduce((acc, num) => acc + num, initVal);
   console.log(result);
   return result;
 };
 
-parsePlusSeparatedExpression(additionEquation);
+parsePlusSeparatedExpression(minusEquation);
