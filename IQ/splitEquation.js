431e7bd8cba47bb68c0af1cd95cec02e2e027f0a
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-07-06 13:51:11 -0500

    fixed splitequation

--- IQ/splitEquation.js
+++ IQ/splitEquation.js
@@ -18,17 +18,47 @@ let minusEquation = "12*5-5*32+3";
 // String split by plus, call minus separator on each index,
 // minus operator splits on - sign and calls multiplication to
 // split on multiplaction sign and run multiplaction
+let parenthesisEquation = "12*5-(5*(32+4))+3";
+
+let split = (expression, operator) => {
+  const result = [];
+  let braces = 0;
+  let currentChunk = "";
+  for (let i = 0; i < expression.length; i++) {
+    const curCh = expression[i];
+    if (curCh == "(") {
+      braces++;
+    } else if (curCh == ")") {
+      braces--;
+    }
+    if (braces == 0 && operator == curCh) {
+      result.push(currentChunk);
+      currentChunk = "";
+    } else currentChunk += curCh;
+  }
+  if (currentChunk != "") {
+    result.push(currentChunk);
+  }
+  return result;
+};
 
 let parseMultiplicationSeparatedExpression = (expression) => {
-  const numberString = expression.split("*");
-  const numbers = numberString.map((noStr) => parseInt(noStr));
+  const numberString = split(expression, "*");
+  const numbers = numberString.map((noStr) => {
+    if (noStr[0] == "(") {
+      const expr = noStr.substr(1, noStr.length - 2);
+
+      return parsePlusSeparatedExpression(expr);
+    }
+    return parseInt(noStr);
+  });
   const initVal = 1.0;
   const result = numbers.reduce((acc, num) => acc * num, initVal);
   return result;
 };
 
 let parseMinusSeparatedExpression = (expression) => {
-  const numbersString = expression.split("-");
+  const numbersString = split(expression, "-");
   const numbers = numbersString.map((noStr) =>
     parseMultiplicationSeparatedExpression(noStr)
   );
@@ -38,7 +68,7 @@ let parseMinusSeparatedExpression = (expression) => {
 };
 
 let parsePlusSeparatedExpression = (expression) => {
-  const numbersString = expression.split("+"); // ["123", "58", "49"]
+  const numbersString = split(expression, "+"); // ["123", "58", "49"]
   console.log(numbersString);
   const numbers = numbersString.map((noStr) =>
     parseMinusSeparatedExpression(noStr)
@@ -50,4 +80,4 @@ let parsePlusSeparatedExpression = (expression) => {
   return result;
 };
 
-parsePlusSeparatedExpression(minusEquation);
+parsePlusSeparatedExpression(parenthesisEquation);
