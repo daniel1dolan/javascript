e91c90fa346c2d8732f41530592c92a73ac00474
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-07-06 15:54:05 -0500

    regex practice

---
+++ REGEX/regex1.js
@@ -0,0 +1,27 @@
+// var patt = /this/;
+
+// console.log(patt.test("this has been"));
+
+// let string = "the best pies are from here";
+// let matchPattern = /e/gi;
+
+// let storage = string.match(matchPattern);
+// console.log(storage.length);
+
+// let result2 = string.replace(matchPattern, "3");
+
+// console.log(result2);
+
+// let pattern = /corgi|dog/g;
+
+// let str = "My dog is a corgi";
+
+// let result = str.match(pattern);
+
+// console.log(result);
+
+// let pattern = /(\d\d\d)-(\d\d\d)-(\d\d\d\d)/;
+// let str = "my phone number is 713-888-2121";
+
+// let result = str.match(pattern);
+// console.log(result);
